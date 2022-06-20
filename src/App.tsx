a1e34318c84fdab3c92b4706e7e3bb3b2f9a066b
Diego Novaes <diego.novaes@marttech.com.br>
2022-06-20 16:23:47 -0300

    feat: troca de tema adicionado, tema escuro criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 
-import { LightTheme } from "./shared/themes";
-
 import { AppRouter } from "./routes";
+import { AppThemeProvider } from "./shared/contexts/";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@mui/material';
 import {Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRouter = () => {
+  const {toggleTheme} = useAppThemeContext();
   return (
     <Routes>
-      <Route path="/home_screen" element={<p>Home Screen</p>} />
+      <Route path="/home_screen" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Teste</Button>} />
       <Route path="*" element={<Navigate to="home_screen"/>} />
     </Routes>
   );
